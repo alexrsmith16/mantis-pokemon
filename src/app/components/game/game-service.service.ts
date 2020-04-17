887983fe6ffe37e7ccdbd4a313fb8f1581301d1f
Hontoro <johnbarlow.bar13@gmail.com>
2020-04-17 10:12:19 -0600

    Added service Files

---
+++ src/app/components/game/game-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+// import {Observable} from 'rxjs/Observable';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GameServiceService {
+
+  constructor(private http: HttpClient) { }
+
+  getPokemon() {
+    return this.http.get("https://api.pokemontcg.io/v1/cards");
+  }
+}
