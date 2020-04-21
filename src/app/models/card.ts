42d4e73b952e3f930a7ba0542ff491fb209bfa85
Hontoro <johnbarlow.bar13@gmail.com>
2020-04-21 10:09:55 -0600

    added model directory

---
+++ src/app/models/card.ts
@@ -0,0 +1,34 @@
+export interface Card {
+    id: string;
+    name: string;
+    nationalPokedexNumber: number;
+    imageUrl: string;
+    imageUrlHiRes: string;
+    types: string[];
+    supertype: string;
+    subtype: string;
+    hp: string;
+    retreatCost: string[];
+    convertedRetreatCost: number;
+    number: string;
+    artist: string;
+    rarity: string;
+    series: string;
+    set: string;
+    setCode: string;
+    attacks: {
+        cost: string[];
+        name: string;
+        text: string;
+        damage: string;
+        convertedEnergyCost: number;
+    }[];
+    resistances: {
+        type: string;
+        value: string;
+    }[]
+    weaknesses: {
+        type: string;
+        value: string;
+    }[]
+}
